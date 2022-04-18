9ef43b15a1958255d6e404469da9f648cdeeae56
Shubham Naugai <71812538+ShubhuNaugai@users.noreply.github.com>
2022-04-19 00:01:12 +0530

    0/1 knapsack (unbounded) -- DP

---
+++ UnboundedKnapsack.py
@@ -0,0 +1,23 @@
+""" 0/1 Knapsack (unbounded) """
+
+max_weight = 7
+weight_array = [1,3,4]
+value_array = [10,5,4]
+n = len(weight_array) # = len(value_array)
+
+## Top-Down
+def UnboundedKnapsack(w, wt, val, n):
+    lookUp = [[-1 for x in range(w + 1)] for x in range(n + 1)]
+    for i in range(n+1):
+        for j in range(w+1):
+            if i == 0 or j == 0:
+                lookUp[i][j] = 0
+            elif wt[i-1] <= j:
+                lookUp[i][j] = max(val[i-1] + lookUp[i][j-wt[i-1]], lookUp[i-1][j])
+            else:
+                lookUp[i][j] =  lookUp[i-1][j]
+    return lookUp[n][w]
+
+print(UnboundedKnapsack(max_weight, weight_array, value_array, n))
+
+
